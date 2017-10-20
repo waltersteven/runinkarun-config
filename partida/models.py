61cefbf84b15bef80528af8616a3bdf6ddfd121a
Walter Pariona <wltrst97@gmail.com>
2017-10-20 17:18:27 -0500

    first commit

---
+++ partida/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Partida(models.Model):
+    # django ignora la id del motor y usa folio
+    nombre = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=10)
+    edad_aproximada = models.IntegerField()
+    fecha_rescate = models.DateField()
+    persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
+    vacuna = models.ManyToManyField(Vacuna, blank=True)
